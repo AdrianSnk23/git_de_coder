d369b9bdad51e7d4f7b9de14d5d3a975015db95d
AdrianSnk23 <rodriguez.adrian23@gmail.com>
2022-10-29 16:18:25 -0300

    arreglos finales de diseño

--- event.js
+++ event.js
@@ -123,7 +123,7 @@ btnReserva.addEventListener("click", () => {
                                 </div>`
 
           } else {
-            contenedorReserva.innerHTML = '<h2 class="text-center mt-3 text-bg-danger">No tiene reserva hecha. Por favor realice su reserva.</h2>'
+            contenedorReserva.innerHTML = '<h2 class="text-center mt-3 text-warning bkgGradient">No tiene reserva hecha. Por favor realice su reserva.</h2>'
           }
           
         })
